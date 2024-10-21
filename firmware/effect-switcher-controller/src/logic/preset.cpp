77058fb59ba83b16d3a71e415aece7e1bcafde7f
yann-ygn <yann@ygn-effects.com>
2024-10-21 22:09:23 +0200

    Refactored the preset logic

--- firmware/effect-switcher-controller/src/logic/preset.cpp
+++ firmware/effect-switcher-controller/src/logic/preset.cpp
@@ -1,102 +1,92 @@
 #include "preset.h"
 
-void Preset::presetSetup(uint8_t bank, uint8_t preset, Loops* loops, uint8_t loopscount)
-{
-    m_bank = bank;
-    m_preset = preset;
-    m_presetLoops = loops;
+void Preset::setup(uint8_t t_bank, uint8_t t_preset, Loops* t_loops, uint8_t t_loopsCount) {
+    m_bank = t_bank;
+    m_preset = t_preset;
+    m_presetLoops = t_loops;
 
-    m_presetLoops->setLoopsCount(loopscount);
+    LOG_DEBUG("Setting up preset: Bank %d, Preset %d", t_bank, t_preset);
+
+    m_presetLoops->setLoopsCount(t_loopsCount);
     m_presetLoops->loopsSetup();
 }
 
-uint8_t Preset::getBank()
-{
+uint8_t Preset::getBank() const {
     return m_bank;
 }
 
-void Preset::setBank(uint8_t bank)
-{
-    m_bank = bank;
+void Preset::setBank(uint8_t t_bank) {
+    m_bank = t_bank;
+    LOG_DEBUG("Bank set to %d", t_bank);
 }
 
-uint8_t* Preset::getPreset()
-{
+uint8_t* Preset::getPreset() {
     return &m_preset;
 }
 
-void Preset::setPreset(uint8_t preset)
-{
-    m_preset = preset;
+void Preset::setPreset(uint8_t t_preset) {
+    m_preset = t_preset;
+    LOG_DEBUG("Preset set to %d", t_preset);
 }
 
-uint8_t* Preset::getPresetLoopsId()
-{
+uint8_t* Preset::getPresetLoopsId() {
     return m_presetLoops->getLoopsId();
 }
 
-uint8_t* Preset::getPresetLoopsStates()
-{
+uint8_t* Preset::getPresetLoopsStates() {
     return m_presetLoops->getLoopsState();
 }
 
-uint8_t* Preset::getPresetLoopsOrder()
-{
+uint8_t* Preset::getPresetLoopsOrder() {
     return m_presetLoops->getLoopsOrder();
 }
 
-void Preset::swapPresetLoopsOrder(uint8_t id1, uint8_t order1, uint8_t id2, uint8_t order2)
-{
-    m_presetLoops->setLoopOrder(id1, order2);
-    m_presetLoops->setLoopOrder(id2, order1);
+void Preset::swapPresetLoopsOrder(uint8_t t_id1, uint8_t t_order1, uint8_t t_id2, uint8_t t_order2) {
+    LOG_DEBUG("Swapping loop order: ID1: %d, Order1: %d, ID2: %d, Order2: %d", t_id1, t_order1, t_id2, t_order2);
+    m_presetLoops->setLoopOrder(t_id1, t_order2);
+    m_presetLoops->setLoopOrder(t_id2, t_order1);
 }
 
-uint8_t Preset::getPresetLoopIdByOrder(uint8_t order)
-{
-    return m_presetLoops->getLoopIdByOrder(order);
+uint8_t Preset::getPresetLoopIdByOrder(uint8_t t_order) {
+    return m_presetLoops->getLoopIdByOrder(t_order);
 }
 
-void Preset::setPresetLoopSendReturn(uint8_t loop, uint8_t send, uint8_t ret)
-{
-    m_presetLoops->setLoopSend(loop, send);
-    m_presetLoops->setLoopReturn(loop, ret);
+void Preset::setPresetLoopSendReturn(uint8_t t_loop, uint8_t t_send, uint8_t t_ret) {
+    LOG_DEBUG("Setting loop send/return: Loop: %d, Send: %d, Return: %d", t_loop, t_send, t_ret);
+    m_presetLoops->setLoopSend(t_loop, t_send);
+    m_presetLoops->setLoopReturn(t_loop, t_ret);
 }
 
-uint8_t Preset::getPresetLoopSend(uint8_t loop)
-{
-    return m_presetLoops->getLoopSend(loop);
+uint8_t Preset::getPresetLoopSend(uint8_t t_loop) {
+    return m_presetLoops->getLoopSend(t_loop);
 }
 
-uint8_t Preset::getPresetLoopReturn(uint8_t loop)
-{
-    return m_presetLoops->getLoopReturn(loop);
+uint8_t Preset::getPresetLoopReturn(uint8_t t_loop) {
+    return m_presetLoops->getLoopReturn(t_loop);
 }
 
-void Preset::setLoopState(uint8_t loop, uint8_t state)
-{
-    m_presetLoops->setLoopState(loop, state);
+void Preset::setLoopState(uint8_t t_loop, uint8_t t_state) {
+    LOG_DEBUG("Setting loop state: Loop %d, State %d", t_loop, t_state);
+    m_presetLoops->setLoopState(t_loop, t_state);
 }
 
-uint8_t Preset::getLoopState(uint8_t loop)
-{
-    return m_presetLoops->getLoopState(loop);
+uint8_t Preset::getLoopState(uint8_t t_loop) {
+    return m_presetLoops->getLoopState(t_loop);
 }
 
-void Preset::toggleLoopState(uint8_t loop)
-{
-    m_presetLoops->toggleLoopsState(loop);
+void Preset::toggleLoopState(uint8_t t_loop) {
+    LOG_DEBUG("Toggling loop state: Loop %d", t_loop);
+    m_presetLoops->toggleLoopsState(t_loop);
 }
 
-uint8_t Preset::getActiveLoopsCount()
-{
-    uint8_t returnValue = 0;
-    for (uint8_t i = 0; i < m_presetLoops->getLoopsCount(); i++)
-    {
-        if (m_presetLoops->getLoopState(i) == 1)
-        {
-            returnValue ++;
+uint8_t Preset::getActiveLoopsCount() {
+    uint8_t count = 0;
+    for (uint8_t i = 0; i < m_presetLoops->getLoopsCount(); i++) {
+        if (m_presetLoops->getLoopState(i) == 1) {
+            count++;
         }
     }
 
-    return returnValue;
+    LOG_DEBUG("Active loops count: %d", count);
+    return count;
 }
--- firmware/effect-switcher-controller/src/logic/preset.h
+++ firmware/effect-switcher-controller/src/logic/preset.h
@@ -1,43 +1,98 @@
+#pragma once
+
 #include <Arduino.h>
 #include "loops.h"
+#include "utils/logging.h"
 
-#ifndef PRESET_H
-#define PRESET_H
+/// @brief Represents a preset that contains a bank, a preset number, and its associated loops.
+class Preset {
+  private:
+    uint8_t m_bank;          // Bank number
+    uint8_t m_preset;        // Preset number
+    Loops* m_presetLoops;    // Pointer to the associated loops
 
-class Preset
-{
-    private:
-        uint8_t m_bank = 0;
-        uint8_t m_preset = 0;
-        Loops* m_presetLoops = NULL;
+  public:
+    /// @brief Default constructor
+    Preset() : m_bank(0), m_preset(0), m_presetLoops(nullptr) { };
 
-    public:
-        Preset() {}
-        Preset(uint8_t bank, uint8_t preset, Loops* loops) :
-            m_bank(bank),
-            m_preset(preset),
-            m_presetLoops(loops) {}
+    /// @brief Parameterized constructor
+    /// @param t_bank Bank number
+    /// @param t_preset Preset number
+    /// @param t_loops Pointer to the loops object
+    Preset(uint8_t t_bank,
+      uint8_t t_preset,
+      Loops* t_loops
+    ) :
+      m_bank(t_bank),
+      m_preset(t_preset),
+      m_presetLoops(t_loops) { };
 
-        void presetSetup(uint8_t bank, uint8_t preset, Loops* loops, uint8_t loopscount);
+    /// @brief Setup the preset with the given bank, preset number, and loops
+    /// @param t_bank Bank number
+    /// @param t_preset Preset number
+    /// @param t_loops Pointer to the loops object
+    /// @param t_loopsCount Number of loops
+    void setup(uint8_t t_bank, uint8_t t_preset, Loops* t_loops, uint8_t t_loopsCount);
 
-        uint8_t getBank();
-        void setBank(uint8_t bank);
+    /// @brief Get the bank number
+    /// @return Bank number
+    uint8_t getBank() const;
 
-        uint8_t* getPreset();
-        void setPreset(uint8_t preset);
+    /// @brief Set the bank number
+    /// @param t_bank Bank number
+    void setBank(uint8_t t_bank);
 
-        uint8_t* getPresetLoopsId();
-        uint8_t* getPresetLoopsStates();
-        uint8_t* getPresetLoopsOrder();
-        void swapPresetLoopsOrder(uint8_t id1, uint8_t order1, uint8_t id2, uint8_t order2);
-        uint8_t getPresetLoopIdByOrder(uint8_t order);
-        void setPresetLoopSendReturn(uint8_t loop, uint8_t send, uint8_t ret);
-        uint8_t getPresetLoopSend(uint8_t loop);
-        uint8_t getPresetLoopReturn(uint8_t loop);
-        void setLoopState(uint8_t loop, uint8_t state);
-        uint8_t getLoopState(uint8_t loop);
-        void toggleLoopState(uint8_t loop);
-        uint8_t getActiveLoopsCount();
+    /// @brief Get the preset number
+    /// @return Pointer to the preset number
+    uint8_t* getPreset();
+
+    /// @brief Set the preset number
+    /// @param t_preset Preset number
+    void setPreset(uint8_t t_preset);
+
+    /// @brief Get the loop IDs for this preset
+    /// @return Pointer to the loop IDs
+    uint8_t* getPresetLoopsId();
+
+    /// @brief Get the loop states for this preset
+    /// @return Pointer to the loop states
+    uint8_t* getPresetLoopsStates();
+
+    /// @brief Get the order of loops for this preset
+    /// @return Pointer to the loop order
+    uint8_t* getPresetLoopsOrder();
+
+    /// @brief Swap the order of two loops in the preset
+    /// @param t_id1 Loop 1 ID
+    /// @param t_order1 Loop 1 order
+    /// @param t_id2 Loop 2 ID
+    /// @param t_order2 Loop 2 order
+    void swapPresetLoopsOrder(uint8_t t_id1, uint8_t t_order1, uint8_t t_id2, uint8_t t_order2);
+
+    /// @brief Get the loop ID based on its order in the preset
+    /// @param t_order Loop order
+    /// @return Loop ID
+    uint8_t getPresetLoopIdByOrder(uint8_t t_order);
+
+    /// @brief Set the send and return state of a loop
+    void setPresetLoopSendReturn(uint8_t t_loop, uint8_t t_send, uint8_t t_ret);
+
+    /// @brief Get the send state of a loop
+    uint8_t getPresetLoopSend(uint8_t t_loop);
+
+    /// @brief Get the return state of a loop
+    uint8_t getPresetLoopReturn(uint8_t t_loop);
+
+    /// @brief Set the state of a loop (on/off)
+    void setLoopState(uint8_t t_loop, uint8_t t_state);
+
+    /// @brief Get the state of a loop (on/off)
+    uint8_t getLoopState(uint8_t t_loop);
+
+    /// @brief Toggle the state of a loop (on/off)
+    void toggleLoopState(uint8_t t_loop);
+
+    /// @brief Get the number of active loops in the preset
+    /// @return Number of active loops
+    uint8_t getActiveLoopsCount();
 };
-
-#endif
