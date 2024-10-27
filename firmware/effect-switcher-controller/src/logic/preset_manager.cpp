261a164509c7fe5a24915e814a43c6023fd8bc44
yann-ygn <yann@ygn-effects.com>
2024-10-27 15:23:02 +0100

    Added the prototype for the preset manager class

---
+++ firmware/effect-switcher-controller/src/logic/preset_manager.cpp
@@ -0,0 +1,81 @@
+#include "logic/preset_manager.h"
+
+void PresetManager::loadPresetBank(uint8_t t_bank) {
+  for (uint8_t i = 0; i < c_maxPresetsPerBank; i++) {
+    m_memoryManager.loadPreset(t_bank, i, m_presetBanks[i]);
+  }
+}
+
+void PresetManager::initialize() {
+  uint8_t lastBank = 0;
+  uint8_t lastPreset = 0;
+  m_memoryManager.loadDeviceState(lastBank, lastPreset);
+  LOG_DEBUG("Last bank: %d, Last preset: %d", lastBank, lastPreset);
+
+  setPresetBank(lastBank);
+  setCurrentPreset(lastPreset);
+}
+
+uint8_t PresetManager::getCurrentBank() const {
+  return m_currentPresetBank;
+}
+
+void PresetManager::setPresetBank(uint8_t t_bank) {
+  if (t_bank < c_maxPresetBanks) {
+    loadPresetBank(t_bank);
+    m_currentPresetBank = t_bank;
+    m_currentPresetIndex = 0;
+    p_currentPreset = &m_presetBanks[m_currentPresetIndex];
+
+    LOG_DEBUG("Current bank: %d, Current preset: %d", m_currentPresetBank, m_currentPresetIndex);
+
+    m_memoryManager.saveDeviceState(m_currentPresetBank, m_currentPresetIndex);
+  } else {
+    LOG_DEBUG("Invalid bank: %d", t_bank);
+  }
+}
+
+void PresetManager::setPresetBankUp() {
+  // If the current bank is the last one, wrap around to 0
+  if ((m_currentPresetBank + 1) >= c_maxPresetBanks) {
+    setPresetBank(0);
+  } else {
+    setPresetBank(m_currentPresetBank + 1);
+  }
+}
+
+void PresetManager::setPresetBankDown() {
+  // If the current bank is the first one, wrap around to the last bank
+  if (m_currentPresetBank == 0) {
+    setPresetBank(c_maxPresetBanks - 1);
+  } else {
+    setPresetBank(m_currentPresetBank - 1);
+  }
+}
+
+Preset* PresetManager::getCurrentPreset() const {
+  return p_currentPreset;
+}
+
+uint8_t PresetManager::getCurrentPresetIndex() const {
+  return m_currentPresetIndex;
+}
+
+void PresetManager::setCurrentPreset(uint8_t t_presetIndex) {
+  if (t_presetIndex < c_maxPresetsPerBank) {
+    m_currentPresetIndex = t_presetIndex;
+    p_currentPreset = &m_presetBanks[m_currentPresetIndex];
+
+    m_memoryManager.saveDeviceState(m_currentPresetBank, m_currentPresetIndex);
+
+    LOG_DEBUG("Current bank: %d, Current preset: %d", m_currentPresetBank, m_currentPresetIndex);
+  }
+  else {
+    LOG_DEBUG("Invalid preset: %d", t_presetIndex);
+  }
+}
+
+void PresetManager::saveCurrentPreset() {
+  m_memoryManager.savePreset(m_currentPresetBank, m_currentPresetIndex, *p_currentPreset);
+  LOG_DEBUG("Saved current preset: Bank %d, Preset %d", m_currentPresetBank, m_currentPresetIndex);
+}
---
+++ firmware/effect-switcher-controller/src/logic/preset_manager.h
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#include "logic/preset.h"
+#include "logic/memory.h"
+
+constexpr uint8_t c_maxPresetBanks = 16;
+constexpr uint8_t c_maxPresetsPerBank = 4;
+
+/// @brief Interface between the HAL and the presets to
+/// securely handle banks and presets switching operations
+/// and storage
+class PresetManager {
+  private:
+    MemoryManager& m_memoryManager;
+
+    uint8_t m_currentPresetBank;
+    uint8_t m_currentPresetIndex;
+
+    Preset m_presetBanks[c_maxPresetsPerBank];
+    Preset* p_currentPreset;
+
+    void loadPresetBank(uint8_t t_bank);
+
+  public:
+    /// @brief Default constructor
+    /// @param t_memoryManager Reference to the MemoryManager object
+    PresetManager(MemoryManager& t_memoryManager) :
+      m_memoryManager(t_memoryManager),
+      m_currentPresetBank(0),
+      m_currentPresetIndex(0),
+      p_currentPreset(nullptr) {
+        for (uint8_t i = 0; i < c_maxPresetsPerBank; i++) {
+          m_presetBanks[i] = Preset();
+        }
+      }
+
+    /// @brief Load the last bank and preset from storage and
+    /// initialize the object's members
+    void initialize();
+
+    /// @brief Get the current bank
+    /// @return uint8_t Current bank
+    uint8_t getCurrentBank() const;
+
+    /// @brief Set the current bank and load it from storage
+    /// @param t_bank Bank number to load
+    void setPresetBank(uint8_t t_bank);
+
+    /// @brief Go up a bank
+    void setPresetBankUp();
+
+    /// @brief Go down a bank
+    void setPresetBankDown();
+
+    /// @brief Get a pointer to the current Preset object
+    /// @return Pointer*
+    Preset* getCurrentPreset() const;
+
+    /// @brief Get the current preset index
+    /// @return uint8_t Preset index
+    uint8_t getCurrentPresetIndex() const;
+
+    /// @brief Set the current preset
+    /// @param t_preset Preset index
+    void setCurrentPreset(uint8_t t_presetIndex);
+
+    /// @brief Save the current preset to storage
+    void saveCurrentPreset();
+};
