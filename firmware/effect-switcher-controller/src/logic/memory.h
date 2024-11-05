82eefd9cc3ed91e9db41d7c6528ee217357951df
yann-ygn <yann@ygn-effects.com>
2024-11-05 18:05:29 +0100

    Added the memory map doc and modified the preset storage constant

--- firmware/effect-switcher-controller/src/logic/memory.h
+++ firmware/effect-switcher-controller/src/logic/memory.h
@@ -6,7 +6,32 @@
 
 constexpr uint16_t c_deviceStateAddress = 0x0;
 constexpr uint16_t c_banksStartAddress = 0x80;
-constexpr uint16_t c_presetSize = 64;
+
+/*
+ * Memory Map for Preset Storage in EEPROM
+ * Total Size per Preset: 128 bytes
+ *
+ * Byte Range       Field Name         Description                             Example Value
+ * -----------------------------------------------------------------------------------------
+ * 0                bank               Stores the preset bank number           1
+ * 1                preset             Stores the preset number within bank    5
+ * 2                loopsCount         Number of audio loops in this preset    16
+ * 3                midiMessagesCount  Number of MIDI messages in this preset  20
+ *
+ * 4-67             loops              Audio loop configuration                (4 bytes per loop)
+ *                   |                   - loopState                           (1 byte each)
+ *                   |                   - loopOrder                           (1 byte each)
+ *                   |                   - loopSend                            (1 byte each)
+ *                   |                   - loopReturn                          (1 byte each)
+ *                  Range: 4 * maxLoops = 4 * 16 = 64 bytes max
+ *
+ * 68-127           midiMessages       MIDI message configuration              (4 bytes per message)
+ *                   |                   - statusByte                          (1 byte each)
+ *                   |                   - dataByte1                           (1 byte each)
+ *                   |                   - dataByte2                           (1 byte each)
+ *                  Range: 4 * maxMIDI = 3 * 20 = 60 bytes max
+ */
+constexpr uint16_t c_presetSize = 128;
 constexpr uint8_t c_presetsPerBank = 4;
 
 class MemoryManager {
