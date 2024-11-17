6c47beac21cb2761a7bfd29f22f653d27371daed
yann-ygn <yann@ygn-effects.com>
2024-11-17 19:15:59 +0100

    First try at converting the menu to the new menu system

--- firmware/effect-switcher-controller/src/logic/list_menu.cpp
+++ firmware/effect-switcher-controller/src/logic/list_menu.cpp
@@ -1,42 +1,89 @@
 #include "list_menu.h"
+#include "utils/logging.h"
 
 void ListMenu::update() {
-  m_display->clear();
-  m_display->renderHeader("Settings");
-  m_display->renderListMenu(m_menuItems, m_itemCount, m_startIndex, m_selectedIndex);
+  m_layoutManager->clear();
+
+  // Visible Content Rows
+  uint8_t endIndex = m_startIndex + m_visibleRowCount;
+  if (endIndex > m_itemCount) {
+    endIndex = m_itemCount;
+  }
+
+  for (uint8_t i = m_startIndex; i < endIndex; i++) {
+    uint8_t rowIndex = 0;
+
+    Row row;
+    row.alignment = Row::kLeft;
+    row.columnsCount = 1;
+    row.columns[0] = { Column::kLabel, Column::kNormal, m_menuItems[i], 0, 0 };
+
+    m_layoutManager->addRow(row);
+
+    m_rowCounts[rowIndex] = i;
+    m_rowColumnCounts[rowIndex] = row.columnsCount;
+    rowIndex++;
+  }
+
+  LOG_DEBUG("Selected row %d:", m_selectedRow);
+  LOG_DEBUG("Selected column %d:", m_selectedColumn);
+
+  m_layoutManager->setActiveRow(m_selectedRow);
+  m_layoutManager->setActiveColumn(m_selectedColumn);
+  m_layoutManager->render();
 }
 
 void ListMenu::reset() {
-  m_selectedIndex = 0;
+  m_selectedRow = 0;
+  m_selectedColumn = 0;
   m_startIndex = 0;
   m_itemSelected = false;
 }
 
 void ListMenu::handleInput(MenuInputAction t_action) {
   switch (t_action) {
-    case MenuInputAction::kUp:
-      if (m_selectedIndex > 0) {
-        m_selectedIndex--;
-        if (m_selectedIndex < m_startIndex) {
+    case MenuInputAction::kUp: {
+      if (m_selectedRow > 0) {
+        if (m_selectedRow == m_startIndex) {
+          // Scroll up when the selected row is at the top of the visible area
           m_startIndex--;
+        } else {
+          // Move the selection up within the visible rows
+          m_selectedRow--;
         }
-      }
-      break;
 
-    case MenuInputAction::kDown:
-      if (m_selectedIndex < m_itemCount - 1) {
-        m_selectedIndex++;
-        if (m_selectedIndex >= m_startIndex + m_display->getMaxVisibleLines()) {
-          m_startIndex++;
+        // Ensure the selected column is within bounds for the new row
+        if (m_rowColumnCounts[m_selectedRow] == 0) {
+          m_selectedColumn = 0;
+        } else if (m_selectedColumn >= m_rowColumnCounts[m_selectedRow]) {
+          m_selectedColumn = m_rowColumnCounts[m_selectedRow] - 1;
         }
       }
       break;
+    }
+
+    case MenuInputAction::kDown: {
+      if (m_selectedRow < m_itemCount - 1) {
+        if (m_selectedRow == m_startIndex + m_visibleRowCount - 1) {
+          // Scroll down when the selected row is at the bottom of the visible area
+          m_startIndex++;
+        } else if (m_selectedRow + m_startIndex < m_itemCount - 1) {
+          // Move the selection down within the visible rows
+          m_selectedRow++;
+        }
+
+        // Ensure the selected column is within bounds for the new row
+        if (m_rowColumnCounts[m_selectedRow] == 0) {
+          m_selectedColumn = 0;
+        } else if (m_selectedColumn >= m_rowColumnCounts[m_selectedRow]) {
+          m_selectedColumn = m_rowColumnCounts[m_selectedRow] - 1;
+        }
+      }
+      break;
+    }
 
     case MenuInputAction::kPress:
       m_itemSelected = true;
-      break;
-
-    case MenuInputAction::kLongPress:
       break;
 
     default:
@@ -45,8 +92,7 @@ void ListMenu::handleInput(MenuInputAction t_action) {
 }
 
 uint8_t ListMenu::getSelectedIndex() {
-  m_itemSelected = false;
-  return m_selectedIndex;
+  return m_startIndex + m_selectedRow;
 }
 
 bool ListMenu::isItemSelected() {
--- firmware/effect-switcher-controller/src/logic/list_menu.h
+++ firmware/effect-switcher-controller/src/logic/list_menu.h
@@ -10,21 +10,30 @@ class ListMenu : public MenuBase {
   private:
     const char** m_menuItems;      // Pointer to an array of menu item strings.
     uint8_t m_itemCount;           // Number of items in the menu.
-    uint8_t m_selectedIndex;       // Index of the currently selected menu item.
+    uint8_t m_selectedRow;         // Currently selected row index.
+    uint8_t m_selectedColumn;      // Currently selected column index.
     uint8_t m_startIndex;          // Index of the first visible item in the menu list.
+    uint8_t m_visibleRowCount;     // Number of rows visible on the screen.
     bool m_itemSelected = false;   // Flag indicating if an item has been selected.
+
+    uint8_t m_rowCounts[c_maxRowsPerLayout];        // Array to track rows.
+    uint8_t m_rowColumnCounts[c_maxRowsPerLayout];  // Array to track columns per row.
+
 
   public:
     /// @brief Constructor to initialize ListMenu with display, items, and item count.
     /// @param t_display Pointer to the Display object used for rendering.
     /// @param t_items Pointer to an array of item strings for the menu.
     /// @param t_count The number of items in the menu.
-    ListMenu(Display* t_display, const char** t_items, uint8_t t_count) :
-      MenuBase(t_display),
+    ListMenu(DisplayManager* t_display, LayoutManager* t_layout, const char** t_items, uint8_t t_count) :
+      MenuBase(t_display, t_layout),
       m_menuItems(t_items),
       m_itemCount(t_count),
-      m_selectedIndex(0),
-      m_startIndex(0) { };
+      m_selectedRow(0),
+      m_selectedColumn(0),
+      m_startIndex(0) {
+        m_visibleRowCount = m_displayManager->getMaxVisibleLines() - 2; // Head + Footer
+      };
 
     /// @brief Updates the display to render the menu, including the header and list of items.
     void update() override;
@@ -36,8 +45,6 @@ class ListMenu : public MenuBase {
     /// @param t_action The input action, such as moving up, down, or selecting an item.
     void handleInput(MenuInputAction t_action) override;
 
-    /// @brief Retrieves the index of the currently selected item, clearing the selection flag.
-    /// @return uint8_t Index of the selected item.
     uint8_t getSelectedIndex();
 
     /// @brief Checks if an item has been selected.
