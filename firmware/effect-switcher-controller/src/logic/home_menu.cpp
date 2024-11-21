2fa7d8d9c550424f66c438701598a759b70f305e
yann-ygn <yann@ygn-effects.com>
2024-11-21 09:20:06 +0100

    Initial work on the home menu using the enw menu system

--- firmware/effect-switcher-controller/src/logic/home_menu.cpp
+++ firmware/effect-switcher-controller/src/logic/home_menu.cpp
@@ -1,7 +1,23 @@
 #include "home_menu.h"
 
 void HomeMenu::update() {
+  m_layoutManager->clear();
 
+  m_layoutManager->setHeader("Current preset");
+
+  char text[4];
+  text[0] = 'A' + m_currentPreset->getBank();
+  text[1] = '-';
+  text[2] = m_currentPreset->getPreset();
+  text[3] = '\0';
+
+  Row row;
+  row.alignment = Row::kFullScreen;
+  row.columnsCount = 1;
+  row.columns[0] = { Column::kLabel, Column::kNormal, text, 0, 0 };
+
+  m_layoutManager->addRow(row);
+  m_layoutManager->render();
 }
 
 void HomeMenu::setCurrentPreset(const Preset* t_currentPreset) {
