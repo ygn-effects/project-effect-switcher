99abb5272000d7bd7258b13bde699cc8926693f7
yann-ygn <yann@ygn-effects.com>
2024-11-22 09:31:23 +0100

    Remmoved unneeded members that have been move to the base class

--- firmware/effect-switcher-controller/src/logic/list_menu.h
+++ firmware/effect-switcher-controller/src/logic/list_menu.h
@@ -9,19 +9,7 @@
 class ListMenu : public MenuBase {
   private:
     const char** m_menuItems;      // Pointer to an array of menu item strings.
-    uint8_t m_itemCount;           // Number of items in the menu.
-    uint8_t m_selectedRow;         // Currently selected row index.
-    uint8_t m_selectedColumn;      // Currently selected column index.
-    uint8_t m_startIndex;          // Index of the first visible item in the menu list.
-    uint8_t m_visibleRowCount;     // Number of rows visible on the screen.
-    bool m_itemSelected = false;   // Flag indicating if an item has been selected.
     bool m_backrequested = false;
-
-    uint8_t m_rowCounts[c_maxRowsPerLayout];        // Array to track rows.
-    uint8_t m_rowColumnCounts[c_maxRowsPerLayout];  // Array to track columns per row.
-
-    bool m_isFooterActive = false;
-    uint8_t m_footerColumnCount = 0;
 
   public:
     /// @brief Constructor to initialize ListMenu with display, items, and item count.
@@ -29,14 +17,7 @@ class ListMenu : public MenuBase {
     /// @param t_items Pointer to an array of item strings for the menu.
     /// @param t_count The number of items in the menu.
     ListMenu(DisplayManager* t_display, LayoutManager* t_layout, const char** t_items, uint8_t t_count) :
-      MenuBase(t_display, t_layout),
-      m_menuItems(t_items),
-      m_itemCount(t_count),
-      m_selectedRow(0),
-      m_selectedColumn(0),
-      m_startIndex(0) {
-        m_visibleRowCount = m_displayManager->getMaxVisibleLines() - 2; // Account for the header
-      };
+      MenuBase(t_display, t_layout) { };
 
     /// @brief Updates the display to render the menu, including the header and list of items.
     void update() override;
