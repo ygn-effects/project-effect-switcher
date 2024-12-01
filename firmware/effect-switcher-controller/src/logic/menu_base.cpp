e1019e886e25a3f4a5c04f1b81d27836c61f4886
yann-ygn <yann@ygn-effects.com>
2024-12-01 15:15:33 +0100

    Handled the menu navigation logic back to the base class

---
+++ firmware/effect-switcher-controller/src/logic/menu_base.cpp
@@ -0,0 +1,91 @@
+#include "logic/menu_base.h"
+
+void MenuBase::navigateMenuUp() {
+  // Check if it's the last row and column and need to enter the footer
+  if (m_selectedRow == m_itemsCount - 1 && m_selectedColumn == m_rowColumnCounts[m_selectedRow] - 1) {
+    enterFooterNavigation();
+    return;
+  }
+
+  // Move within the current row or to the next row
+  if (m_selectedColumn == m_rowColumnCounts[m_selectedRow] - 1) {
+    m_selectedRow++;
+    m_selectedColumn = 0;
+  } else {
+    m_selectedColumn++;
+  }
+
+  // Scroll if we move past the visible rows
+  if (m_selectedRow >= m_startIndex + m_visibleRowCount) {
+    m_startIndex++;
+  }
+}
+
+void MenuBase::navigateMenuDown() {
+  // Move within the current row or to the previous row
+  if (m_selectedColumn == 0) {
+      if (m_selectedRow > 0) {
+        m_selectedRow--;
+        m_selectedColumn = m_rowColumnCounts[m_selectedRow] - 1;
+      }
+  } else {
+    m_selectedColumn--;
+  }
+
+  // Scroll if we move past the visible rows
+  if (m_selectedRow < m_startIndex) {
+    m_startIndex--;
+  }
+}
+
+void MenuBase::navigateFooterUp() {
+  if (m_selectedColumn < m_footerColumnCount - 1) {
+    m_selectedColumn++;
+  }
+}
+
+void MenuBase::navigateFooterDown() {
+  if (m_selectedColumn > 0) {
+    m_selectedColumn--;
+  } else {
+    exitFooterNavigation();
+  }
+}
+
+void MenuBase::enterFooterNavigation() {
+  m_isFooterActive = true;
+  m_layoutManager->setIsFooterActive(true);
+  m_selectedColumn = 0;  // Start at the first footer column
+}
+
+void MenuBase::exitFooterNavigation() {
+  m_isFooterActive = false;
+  m_layoutManager->setIsFooterActive(false);
+  m_selectedRow = m_itemsCount - 1;
+  m_selectedColumn = m_rowColumnCounts[m_selectedRow] - 1;  // Last column of the last row
+}
+
+void MenuBase::handleNavigation(MenuInputAction t_action) {
+  switch (t_action) {
+    case MenuInputAction::kUp:
+      if (m_isFooterActive) {
+        navigateFooterUp();
+      } else {
+        navigateMenuUp();
+      }
+      break;
+
+    case MenuInputAction::kDown:
+      if (m_isFooterActive) {
+        navigateFooterDown();
+      } else {
+        navigateMenuDown();
+      }
+      break;
+
+    case MenuInputAction::kPress:
+    case MenuInputAction::kLongPress:
+    default:
+        break;
+  }
+}
--- firmware/effect-switcher-controller/src/logic/menu_base.h
+++ firmware/effect-switcher-controller/src/logic/menu_base.h
@@ -17,10 +17,11 @@ class MenuBase {
     DisplayManager* m_displayManager;               // Display manager instance
     LayoutManager* m_layoutManager;                 // Layout manager instance
 
-    uint8_t m_visibleRowCount = 0;                  // Number of rows visible on the screen.
-    uint8_t m_selectedRow = 0;                      // Currently selected row index.
-    uint8_t m_selectedColumn = 0;                   // Currently selected column index.
-    uint8_t m_startIndex = 0;                       // Index of the first visible item in the menu list.
+    uint8_t m_visibleRowCount;                  // Number of rows visible on the screen.
+    uint8_t m_selectedRow;                      // Currently selected row index.
+    uint8_t m_selectedColumn;                   // Currently selected column index.
+    uint8_t m_startIndex;                       // Index of the first visible item in the menu list.
+    uint8_t m_itemsCount;
 
     uint8_t m_rowCounts[LayoutConstants::c_maxRowsPerLayout];        // Array to track rows.
     uint8_t m_rowColumnCounts[LayoutConstants::c_maxColumnsPerRow];  // Array to track columns per row.
@@ -29,13 +30,34 @@ class MenuBase {
     bool m_isFooterActive = false;
     bool m_itemSelected = false;                    // Flag indicating if an item has been selected.
 
+    void navigateMenuUp();
+    void navigateMenuDown();
+    void navigateFooterUp();
+    void navigateFooterDown();
+    void enterFooterNavigation();
+    void exitFooterNavigation();
+
   public:
     /// @brief Constructor for the base menu
     /// @param t_displayWidth Width of the display
     /// @param t_displayHeight Height of the display
     MenuBase(DisplayManager* t_display, LayoutManager* t_layout) :
       m_displayManager(t_display),
-      m_layoutManager(t_layout) {
+      m_layoutManager(t_layout),
+      m_selectedRow(0),
+      m_selectedColumn(0),
+      m_startIndex(0),
+      m_itemsCount(0) {
+        m_visibleRowCount = m_displayManager->getMaxVisibleLines() - 2; // Account for the header and footer
+      };
+
+    MenuBase(DisplayManager* t_display, LayoutManager* t_layout, uint8_t t_count) :
+      m_displayManager(t_display),
+      m_layoutManager(t_layout),
+      m_selectedRow(0),
+      m_selectedColumn(0),
+      m_startIndex(0),
+      m_itemsCount(t_count) {
         m_visibleRowCount = m_displayManager->getMaxVisibleLines() - 2; // Account for the header and footer
       };
 
@@ -44,6 +66,10 @@ class MenuBase {
 
     /// @brief Reset the menu to its initial state
     virtual void reset() = 0;
+
+    void handleNavigation(MenuInputAction t_action);
+
+    virtual void handleAction(MenuInputAction t_action) = 0;
 
     /// @brief Handle user input (e.g., button press, encoder turn)
     virtual void handleInput(MenuInputAction t_action) = 0;
