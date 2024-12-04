3788ac6c01e2a5f75e11daac842ce7c7df05ab5d
yann-ygn <yann@ygn-effects.com>
2024-12-04 22:13:58 +0100

    Rewrite of the MIDI menu and MIDI menu edit menus

--- firmware/effect-switcher-controller/src/logic/midi_menu.cpp
+++ firmware/effect-switcher-controller/src/logic/midi_menu.cpp
@@ -1,23 +1,123 @@
 #include "midi_menu.h"
 
 void MidiMessageMenu::update() {
+  m_layoutManager->clear();
 
+  // Set header
+  m_layoutManager->setHeader("MIDI messages");
+
+  // Set footer
+  m_footerColumnCount = 2;
+  const char* footItems[] = {"Add", "Back"};
+  m_layoutManager->setFooter(footItems, m_footerColumnCount);
+
+  // Visible Content Rows
+  m_itemsCount = m_presetView->midiMessagesCount;
+  uint8_t endIndex = m_startIndex + m_visibleRowCount;
+  if (endIndex > m_itemsCount) {
+    endIndex = m_itemsCount;
+  }
+
+  uint8_t rowIndex = 0;
+  for (uint8_t i = m_startIndex; i < endIndex; i++) {
+    Row row;
+    row.alignment = Row::kLeft;
+    row.columnsCount = 4;
+
+    MidiMessageView& message = m_presetView->midiMessages[i];
+
+    StaticString type;
+    StaticString channel(message.channel);
+    StaticString byte1(message.byte1);
+    StaticString byte2(message.byte2);
+
+    switch (message.type) {
+      case 0xB0:
+        type.append('C');
+        type.append('C');
+        break;
+
+      case 0xC0:
+        type.append('P');
+        type.append('C');
+        break;
+
+      default:
+        break;
+    }
+
+    row.columns[0] = { Column::kLabel, Column::kNormal, "T:", type.c_str(), 0 };
+    row.columns[1] = { Column::kLabel, Column::kNormal, "C:", channel.c_str(), 0 };
+    row.columns[2] = { Column::kLabel, Column::kNormal, "B1:", byte1.c_str(), 0 };
+
+    if (message.hasDataByte2) {
+      row.columns[3] = { Column::kLabel, Column::kNormal, "B2:", byte2.c_str(), 0 };
+    }
+    else {
+      row.columns[3] = { Column::kLabel, Column::kNormal, "B2:", nullptr, 0 };
+    }
+
+    m_layoutManager->addRow(row);
+
+    m_rowCounts[rowIndex] = i;
+    m_rowColumnCounts[rowIndex] = 1;
+
+    rowIndex++;
+  }
+
+  m_layoutManager->setActiveRow(m_selectedRow);
+  m_layoutManager->setActiveColumn(m_selectedColumn);
+  m_layoutManager->render();
 }
 
 void MidiMessageMenu::reset() {
+  m_selectedRow = 0;
+  m_selectedColumn = 0;
+  m_startIndex = 0;
+  m_isFooterActive = false;
   m_editRequested = false;
   m_addRequested = false;
-  m_goBackRequested = false;
-  m_selectedIndex = 0;
-  m_startIndex = 0;
+  m_backRequested = false;
+  m_itemSelected = false;
+  m_layoutManager->setIsFooterActive(false);
+  m_layoutManager->setActiveRow(m_selectedRow);
+  m_layoutManager->setActiveColumn(m_selectedColumn);
 }
 
-void MidiMessageMenu::handleInput(MenuInputAction t_action) {
+void MidiMessageMenu::handleAction(MenuInputAction t_action) {
+  switch (t_action) {
+    case MenuInputAction::kPress:
+      if (m_isFooterActive) {
+        switch (m_selectedColumn) {
+          case 0:
+            m_addRequested = true;
+            break;
 
-}
+          case 1:
+            m_backRequested = true;
+            break;
 
-void MidiMessageMenu::setCurrentPreset(Preset* t_currentPreset) {
-  m_currentPreset = t_currentPreset;
+          default:
+            break;
+        }
+      }
+      else {
+        m_editRequested = true;
+      }
+      break;
+
+    case MenuInputAction::kUp:
+      handleNavigation(t_action);
+      break;
+
+    case MenuInputAction::kDown:
+      handleNavigation(t_action);
+      break;
+
+    case MenuInputAction::kLongPress:
+    default:
+      break;
+  }
 }
 
 bool MidiMessageMenu::isEditRequested() {
@@ -34,26 +134,94 @@ bool MidiMessageMenu::isAddRequested() {
   return requested;
 }
 
-bool MidiMessageMenu::isGoBackRequested() {
-  bool requested = m_goBackRequested;
-  m_goBackRequested = false;
+bool MidiMessageMenu::isBackRequested() {
+  bool requested = m_backRequested;
+  m_backRequested = false;
 
   return requested;
 }
 
-uint8_t MidiMessageMenu::getSelectedIndex() const {
-  return m_selectedIndex;
-}
-
-uint8_t MidiMessageMenu::getStartIndex() const {
-  return m_startIndex;
-}
-
 void MidiMessageEditMenu::update() {
+  m_layoutManager->clear();
 
+  // Set header
+  m_layoutManager->setHeader("MIDI message edit");
+
+  // Set footer
+  m_footerColumnCount = 2;
+
+  if (m_messageEditMode) {
+    const char* footItems[] = {"Cancel", "Save"};
+    m_layoutManager->setFooter(footItems, m_footerColumnCount);
+  }
+  else {
+    const char* footItems[] = {"Cancel", "Add"};
+    m_layoutManager->setFooter(footItems, m_footerColumnCount);
+  }
+
+  StaticString type;
+  StaticString channel(m_newMessageChannel);
+  StaticString byte1(m_newMessageDataByte1);
+  StaticString byte2(m_newMessageDataByte2);
+
+  switch (m_newMessageType) {
+    case 0xB0:
+      type.append('C');
+      type.append('C');
+      break;
+
+    case 0xC0:
+      type.append('P');
+      type.append('C');
+      break;
+
+    default:
+      break;
+  }
+
+  uint8_t rowIndex = 0;
+  for (uint8_t i = 0; i < 2; i++) {
+    Row row;
+    row.alignment = Row::kCenter;
+    row.columnsCount = 2;
+
+    auto getColumnStyle = [this, rowIndex](uint8_t columnIndex) {
+      return (m_selectedRow == rowIndex && m_selectedColumn == columnIndex && !m_isNavigationActive)
+                  ? Column::kValueHighLighted
+                  : Column::kNormal;
+    };
+
+    if (rowIndex == 0) {
+      row.columns[0] = { Column::kLabel, getColumnStyle(0), "T:", type.c_str(), 0 };
+      row.columns[1] = { Column::kLabel, getColumnStyle(1), "C:", channel.c_str(), 0 };
+    }
+    else {
+      row.columns[0] = { Column::kLabel, getColumnStyle(0), "B1:", byte1.c_str(), 0 };
+
+      if (m_NewMessageHasDataByte2) {
+        row.columns[1] = { Column::kLabel, getColumnStyle(1), "B2:", byte2.c_str(), 0 };
+      }
+      else {
+        row.columns[1] = { Column::kLabel, getColumnStyle(1), "B2:", nullptr, 0 };
+      }
+    }
+
+    m_layoutManager->addRow(row);
+
+    m_rowCounts[rowIndex] = i;
+    m_rowColumnCounts[rowIndex] = row.columnsCount;
+    rowIndex++;
+    m_itemsCount = rowIndex;
+  }
+
+  m_layoutManager->setActiveRow(m_selectedRow);
+  m_layoutManager->setActiveColumn(m_selectedColumn);
+  m_layoutManager->render();
 }
 
 void MidiMessageEditMenu::reset() {
+  m_selectedRow = 0;
+  m_selectedColumn = 0;
   m_midiMessageIndex = 0;
   m_messageEditMode = false;
   m_fieldEditMode = false;
@@ -67,18 +235,110 @@ void MidiMessageEditMenu::reset() {
   m_newMessageDataByte1 = 64;
   m_newMessageDataByte2 = 64;
   m_NewMessageHasDataByte2 = true;
+  m_layoutManager->setIsFooterActive(false);
+  m_layoutManager->setActiveRow(m_selectedRow);
+  m_layoutManager->setActiveColumn(m_selectedColumn);
 }
 
-void MidiMessageEditMenu::handleInput(MenuInputAction t_action) {
+void MidiMessageEditMenu::handleAction(MenuInputAction t_action) {
+  switch (t_action) {
+    case MenuInputAction::kUp:
+      if (m_isNavigationActive) {
+        handleNavigation(t_action);
+      }
+      else {
+        switch (m_selectedRow * 2 + m_selectedColumn) {
+          case 0:
+            break;
 
+          case 1:
+            m_newMessageChannel=+ (m_newMessageChannel + 1) % 16;
+            break;
+
+          case 2:
+            m_newMessageDataByte1 = (m_newMessageDataByte1 + 1) % 128;
+            break;
+
+          case 3:
+            if (m_NewMessageHasDataByte2) {
+              m_newMessageDataByte2 = (m_newMessageDataByte2 + 1) % 128;
+            }
+            break;
+
+          default:
+            break;
+        }
+      }
+      break;
+
+    case MenuInputAction::kDown:
+      if (m_isNavigationActive) {
+        handleNavigation(t_action);
+      }
+      else {
+        switch (m_selectedRow * 2 + m_selectedColumn) {
+          case 0:
+            break;
+
+          case 1:
+            m_newMessageChannel = (m_newMessageChannel - 1) % 16;
+            break;
+
+          case 2:
+            m_newMessageDataByte1 = (m_newMessageDataByte1 - 1) % 128;
+            break;
+
+          case 3:
+            if (m_NewMessageHasDataByte2) {
+              m_newMessageDataByte2 = (m_newMessageDataByte2 - 1) % 128;
+            }
+            break;
+
+          default:
+            break;
+        }
+      }
+      break;
+
+    case MenuInputAction::kPress:
+      if (!m_isFooterActive) {
+        m_isNavigationActive = !m_isNavigationActive;
+      }
+      else {
+        switch (m_selectedColumn) {
+          case 0:
+            m_cancelRequested = true;
+            break;
+
+          case 1:
+            if (m_messageEditMode) {
+              m_addRequested = true;
+            }
+            else {
+              m_saveRequested = true;
+            }
+            break;
+
+          default:
+            break;
+        }
+      }
+      break;
+
+    case MenuInputAction::kLongPress:
+      break;
+
+    default:
+      break;
+  }
+}
+
+void MidiMessageEditMenu::setPresetView(PresetView* t_view) {
+  m_presetView = t_view;
 }
 
 uint8_t MidiMessageEditMenu::getMidiMessageIndex() {
   return m_midiMessageIndex;
-}
-
-void MidiMessageEditMenu::setCurrentPreset(Preset* t_currentPreset) {
-  m_currentPreset = t_currentPreset;
 }
 
 void MidiMessageEditMenu::setMidiMessageIndex(uint8_t t_index) {
@@ -88,13 +348,13 @@ void MidiMessageEditMenu::setMidiMessageIndex(uint8_t t_index) {
 void MidiMessageEditMenu::setMessageEditMode(bool t_mode) {
   m_messageEditMode = t_mode;
 
-  // Edit mode, retrieve data from the current message to the local members
   if (m_messageEditMode) {
-    m_newMessageType = m_currentPreset->getMidiMessageType(m_midiMessageIndex);
-    m_newMessageChannel = m_currentPreset->getMidiMessageChannel(m_midiMessageIndex);
-    m_newMessageDataByte1 = m_currentPreset->getMidiMessageDataByte1(m_midiMessageIndex);
-    m_newMessageDataByte2 = m_currentPreset->getMidiMessageDataByte2(m_midiMessageIndex);
-    m_NewMessageHasDataByte2 = m_currentPreset->getMidiMessageHasDataByte2(m_midiMessageIndex);
+    const MidiMessageView message = m_presetView->midiMessages[m_midiMessageIndex];
+    m_newMessageType = message.type;
+    m_newMessageChannel = message.channel;
+    m_newMessageDataByte1 = message.byte1;
+    m_newMessageDataByte2 = message.byte2;
+    m_NewMessageHasDataByte2 = message.hasDataByte2;
   }
 }
 
@@ -124,24 +384,4 @@ bool MidiMessageEditMenu::isAddRequested() {
   m_addRequested = false;
 
   return requested;
-}
-
-uint8_t MidiMessageEditMenu::getNewMessageType() const {
-  return m_newMessageType;
-}
-
-uint8_t MidiMessageEditMenu::getNewMessageChannel() const {
-  return m_newMessageChannel;
-}
-
-uint8_t MidiMessageEditMenu::getNewMessageDataByte1() const {
-  return m_newMessageDataByte1;
-}
-
-uint8_t MidiMessageEditMenu::getNewMessageDataByte2() const {
-  return m_newMessageDataByte2;
-}
-
-bool MidiMessageEditMenu::getNewMessageHasDataByte2() const {
-  return m_NewMessageHasDataByte2;
 }
--- firmware/effect-switcher-controller/src/logic/midi_menu.h
+++ firmware/effect-switcher-controller/src/logic/midi_menu.h
@@ -2,7 +2,7 @@
 
 #include "menu_base.h"
 #include "peripherals/display.h"
-#include "logic/preset.h"
+#include "utils/static_string.h"
 
 /// @brief Class representing the MIDI message menu, allowing the user to view, add, or edit MIDI messages.
 ///
@@ -10,24 +10,19 @@
 /// select a message for editing, add a new message, or go back to the previous menu.
 class MidiMessageMenu : public MenuBase {
   private:
-    Preset* m_currentPreset;        // Pointer to the currently selected Preset.
-    uint8_t m_selectedIndex;        // Index of the currently selected MIDI message.
-    uint8_t m_startIndex;           // Index of the first visible item in the list.
     bool m_editRequested;           // Flag indicating if an edit action is requested.
     bool m_addRequested;            // Flag indicating if an add action is requested.
-    bool m_goBackRequested;         // Flag indicating if a go-back action is requested.
+    bool m_backRequested;         // Flag indicating if a go-back action is requested.
 
   public:
     /// @brief Constructor to initialize the MIDI message menu with a display and preset.
     /// @param t_display Pointer to the Display object for rendering.
     /// @param t_currentPreset Pointer to the current Preset containing MIDI messages.
-    MidiMessageMenu(DisplayManager* t_display, LayoutManager* t_layout, Preset* t_currentPreset) :
+    MidiMessageMenu(DisplayManager* t_display, LayoutManager* t_layout) :
       MenuBase(t_display, t_layout),
-      m_currentPreset(t_currentPreset),
-      m_selectedIndex(0),
-      m_startIndex(0),
       m_editRequested(false),
-      m_goBackRequested(false) { };
+      m_addRequested(false),
+      m_backRequested(false) { };
 
     /// @brief Updates the display to render the list of MIDI messages.
     void update() override;
@@ -35,13 +30,7 @@ class MidiMessageMenu : public MenuBase {
     /// @brief Resets the menu's state, clearing selection flags and resetting indices.
     void reset() override;
 
-    /// @brief Handles user input actions to navigate and select options within the menu.
-    /// @param t_action The input action, such as moving up, down, or selecting an item.
-    void handleInput(MenuInputAction t_action) override;
-
-    /// @brief Sets the current preset.
-    /// @param t_currentPreset Pointer to the current Preset.
-    void setCurrentPreset(Preset* t_currentPreset);
+    void handleAction(MenuInputAction t_action) override;
 
     /// @brief Checks if an edit action was requested.
     /// @return true if an edit action is requested; false otherwise.
@@ -53,15 +42,7 @@ class MidiMessageMenu : public MenuBase {
 
     /// @brief Checks if a go-back action was requested.
     /// @return true if a go-back action is requested; false otherwise.
-    bool isGoBackRequested();
-
-    /// @brief Retrieves the index of the currently selected item.
-    /// @return uint8_t The index of the selected item.
-    uint8_t getSelectedIndex() const;
-
-    /// @brief Retrieves the start index of the visible items in the list.
-    /// @return uint8_t The start index of the visible items.
-    uint8_t getStartIndex() const;
+    bool isBackRequested();
 };
 
 /// @brief Class representing the edit menu for a MIDI message, allowing modification of individual fields.
@@ -70,16 +51,17 @@ class MidiMessageMenu : public MenuBase {
 /// channel, data bytes, and other settings.
 class MidiMessageEditMenu : public MenuBase {
   private:
-    Preset* m_currentPreset;              // Pointer to the current Preset object.
+    PresetView* m_presetView;             // Pointer to the current Preset object.
     uint8_t m_midiMessageIndex;           // Index of the MIDI message being edited.
     uint8_t m_selectedIndex;              // Index of the currently selected field in the message.
+
     bool m_messageEditMode;               // Flag indicating if edit mode is active for a MIDI message.
     bool m_fieldEditMode;                 // Flag indicating if field edit mode is active.
     bool m_cancelRequested;               // Flag indicating if a cancel action was requested.
     bool m_deleteRequested;               // Flag indicating if a delete action was requested.
     bool m_saveRequested;                 // Flag indicating if a save action was requested.
     bool m_addRequested;                  // Flag indicating if an add action was requested.
-    uint8_t m_messagesCount = 4;          // Number of MIDI messages.
+
     uint8_t m_newMessageType = 0xB0;      // Default value for the MIDI message type.
     uint8_t m_newMessageChannel = 0;      // Default value for the MIDI message channel.
     uint8_t m_newMessageDataByte1 = 64;   // Default value for the first data byte.
@@ -91,9 +73,8 @@ class MidiMessageEditMenu : public MenuBase {
     /// @param t_display Pointer to the Display object.
     /// @param t_currentPreset Pointer to the current Preset.
     /// @param t_midiMessageIndex Index of the MIDI message to edit.
-    MidiMessageEditMenu(DisplayManager* t_display, LayoutManager* t_layout, Preset* t_currentPreset, uint8_t t_midiMessageIndex) :
+    MidiMessageEditMenu(DisplayManager* t_display, LayoutManager* t_layout, uint8_t t_midiMessageIndex) :
       MenuBase(t_display, t_layout),
-      m_currentPreset(t_currentPreset),
       m_midiMessageIndex(t_midiMessageIndex),
       m_selectedIndex(0),
       m_messageEditMode(false),
@@ -107,13 +88,9 @@ class MidiMessageEditMenu : public MenuBase {
     /// @brief Resets the edit menu, clearing all flags and resetting fields.
     void reset() override;
 
-    /// @brief Handles user input actions to navigate and modify fields within the edit menu.
-    /// @param t_action The input action, such as selecting a field or modifying a value.
-    void handleInput(MenuInputAction t_action) override;
+    void handleAction(MenuInputAction t_action) override;
 
-    /// @brief Sets the current preset.
-    /// @param t_currentPreset Pointer to the Preset containing the MIDI message to edit.
-    void setCurrentPreset(Preset* t_currentPreset);
+    void setPresetView(PresetView* t_view);
 
     /// @brief Retrieves the index of the MIDI message being edited.
     /// @return uint8_t The index of the MIDI message.
@@ -142,24 +119,4 @@ class MidiMessageEditMenu : public MenuBase {
     /// @brief Checks if an add action was requested.
     /// @return true if add action was requested; false otherwise.
     bool isAddRequested();
-
-    /// @brief Retrieves the type of the new MIDI message.
-    /// @return uint8_t The type of the new MIDI message.
-    uint8_t getNewMessageType() const;
-
-    /// @brief Retrieves the channel of the new MIDI message.
-    /// @return uint8_t The channel of the new MIDI message.
-    uint8_t getNewMessageChannel() const;
-
-    /// @brief Retrieves the first data byte of the new MIDI message.
-    /// @return uint8_t The first data byte.
-    uint8_t getNewMessageDataByte1() const;
-
-    /// @brief Retrieves the second data byte of the new MIDI message.
-    /// @return uint8_t The second data byte.
-    uint8_t getNewMessageDataByte2() const;
-
-    /// @brief Checks if the new MIDI message has a second data byte.
-    /// @return true if there is a second data byte; false otherwise.
-    bool getNewMessageHasDataByte2() const;
 };
