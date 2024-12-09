387324ef787313a260faa35bd61322a033f497dc
yann-ygn <yann@ygn-effects.com>
2024-12-09 21:17:48 +0100

    Changed to a new screen

--- firmware/effect-switcher-controller/src/logic/display_manager.cpp
+++ firmware/effect-switcher-controller/src/logic/display_manager.cpp
@@ -1,23 +1,25 @@
 #include "display_manager.h"
 
 void DisplayManager::setup() {
-  m_ssd1306.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  m_st7789.init(m_height, m_width);
+  m_st7789.setSPISpeed(8000000);
+  m_st7789.setRotation(1);
 }
 
 void DisplayManager::reset() {
-  m_ssd1306.clearDisplay();
-  m_ssd1306.setFont();
-  m_ssd1306.setTextColor(SSD1306_WHITE);
+  m_st7789.fillScreen(ST77XX_BLACK);
+  m_st7789.setFont();
+  m_st7789.setTextColor(ST77XX_WHITE);
 }
 
 void DisplayManager::render() {
-  m_ssd1306.display();
+
 }
 
 uint16_t DisplayManager::calcTextWidth(const char* t_text) {
   int16_t x1, y1;
   uint16_t textWidth, height;
-  m_ssd1306.getTextBounds(t_text, 0, 0, &x1, &y1, &textWidth, &height);
+  m_st7789.getTextBounds(t_text, 0, 0, &x1, &y1, &textWidth, &height);
 
   return textWidth;
 }
@@ -25,16 +27,16 @@ uint16_t DisplayManager::calcTextWidth(const char* t_text) {
 uint16_t DisplayManager::calcTextHeight(const char* t_text) {
   int16_t x1, y1;
   uint16_t textWidth, height;
-  m_ssd1306.getTextBounds(t_text, 0, 0, &x1, &y1, &textWidth, &height);
+  m_st7789.getTextBounds(t_text, 0, 0, &x1, &y1, &textWidth, &height);
 
   return height;
 }
 
-uint8_t DisplayManager::getWidth() {
+uint16_t DisplayManager::getWidth() {
   return m_width;
 }
 
-uint8_t DisplayManager::getHeight() {
+uint16_t DisplayManager::getHeight() {
   return m_height;
 }
 
@@ -51,13 +53,13 @@ uint8_t DisplayManager::getMaxVisibleLines() {
 }
 
 void DisplayManager::printItem(const char* t_text, uint8_t t_x, uint8_t t_y) {
-  m_ssd1306.setCursor(t_x, t_y);
-  m_ssd1306.write(t_text);
+  m_st7789.setCursor(t_x, t_y);
+  m_st7789.write(t_text);
 }
 
 void DisplayManager::printFullScreenItem(const char* t_text, uint8_t t_usableHeight, uint8_t t_usableWidth) {
   for (uint8_t fontSize = 16; fontSize >= 0; fontSize -= 2) {
-    m_ssd1306.setTextSize(fontSize);
+    m_st7789.setTextSize(fontSize);
 
     uint16_t textHeight = calcTextHeight(t_text);
     uint16_t textWidth = calcTextWidth(t_text);
@@ -67,19 +69,19 @@ void DisplayManager::printFullScreenItem(const char* t_text, uint8_t t_usableHei
       uint8_t yPosition = (m_width - textWidth) / 2;
 
       printItem(t_text, xPosition, yPosition);
-      m_ssd1306.setTextSize(0);
+      m_st7789.setTextSize(m_fontSize);
       return;
     }
   }
 }
 
 void DisplayManager::printHighlightedItem(const char* t_text, uint8_t t_x, uint8_t t_y) {
-  m_ssd1306.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
+  m_st7789.setTextColor(ST77XX_BLACK, ST77XX_WHITE);
   printItem(t_text, t_x, t_y);
-  m_ssd1306.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
+  m_st7789.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
 }
 
 void DisplayManager::drawInvertedLine(uint8_t t_y) {
-  m_ssd1306.fillRect(0, 0, m_width, m_newLine, SSD1306_WHITE);
-  m_ssd1306.setTextColor(SSD1306_WHITE);
+  m_st7789.fillRect(0, 0, m_width, m_newLine, ST77XX_WHITE);
+  m_st7789.setTextColor(ST77XX_WHITE);
 }
--- firmware/effect-switcher-controller/src/logic/display_manager.h
+++ firmware/effect-switcher-controller/src/logic/display_manager.h
@@ -2,23 +2,25 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_ST7789.h>
 #include <Adafruit_GFX.h>
 
 
 class DisplayManager {
   private:
-    Adafruit_SSD1306 m_ssd1306;
-    uint8_t m_width;
-    uint8_t m_height;
+    Adafruit_ST7789 m_st7789;
+    uint16_t m_width;
+    uint16_t m_height;
     uint8_t m_newLine;
     uint8_t m_newTab;
+    uint8_t m_fontSize = 2;
 
   public:
-    DisplayManager(uint8_t t_width, uint8_t t_height) :
-      m_ssd1306(t_width, t_height, &Wire, -1),
+    DisplayManager(uint16_t t_width, uint16_t t_height, uint8_t t_cs, uint8_t t_dc, uint8_t t_rst) :
+      m_st7789(t_cs, t_dc, t_rst),
       m_width(t_width),
       m_height(t_height) {
+        m_st7789.setTextSize(m_fontSize);
         m_newTab = calcTextWidth("A");
         m_newLine = calcTextHeight("A");
       }
@@ -33,9 +35,9 @@ class DisplayManager {
 
     uint16_t calcTextHeight(const char* t_text);
 
-    uint8_t getWidth();
+    uint16_t getWidth();
 
-    uint8_t getHeight();
+    uint16_t getHeight();
 
     uint8_t getNewLine();
 
